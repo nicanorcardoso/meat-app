8ef980e47966b0cbc952aaa7d3b4d37c728a250d
nicanor <nicanor@localhost.localdomain>
2019-02-27 07:42:13 -0300

    enviando compra para o back end

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -2,12 +2,15 @@ import { Component, OnInit } from '@angular/core';
 import { RadioOption } from 'app/shared/radio/radio-option.model';
 import { OrderService } from './order.services';
 import { CartItem } from 'app/restaurant-detail/shopping-cart/cart-item.model';
+import {Order, OrderItem} from './order.model'
 
 @Component({
   selector: 'mt-order',
   templateUrl: './order.component.html',
 })
 export class OrderComponent implements OnInit {
+
+  delivery: number = 8
 
   paymentOptions: RadioOption[] = [
     {label: 'Dinheiro', value: 'MON'},
@@ -18,6 +21,10 @@ export class OrderComponent implements OnInit {
   constructor(private orderService: OrderService) { }
 
   ngOnInit() {
+  }
+
+  itemsValue(): number{
+    return this.orderService.itemsValue()
   }
   cartItems(): CartItem[]{
     return this.orderService.cartItems()
@@ -30,5 +37,10 @@ export class OrderComponent implements OnInit {
   }
   remove(item: CartItem){
     this.orderService.remove(item)
+  }
+  checkOrder(order: Order){
+    order.orderItems = this.cartItems().map((item:CartItem)=>new OrderItem(item.quantity, item.menuItem.id))
+    this.orderService.checkOrder(order)
+    console.log(order)
   }
 }
